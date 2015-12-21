5b778f70949377abb8144bf85883155283e8f1ab
kuwala <kuwala@gmail.com>
2015-12-20 22:43:38 -0800

    trying to investigate slow runTime Counter

--- longRecModel.py
+++ longRecModel.py
@@ -13,13 +13,15 @@ class LongRecModel(object):
       self.drawState = "draw"
   def updateRunTime(self, millisecs):
     # time 
+    """
     hundredths = millisecs / 10
     hundredths = hundredths % 100
     secs = millisecs / 1000
     secs = secs % 60
     min = millisecs / 60000
     text = str(min) + ":" + str(secs).zfill(2)+"." + str(hundredths).zfill(2)
-    print("runtime text %s" % text)
+    """
+    text = str(millisecs)
     self.runTime = text
   def makeActive(self):
     self.drawState = "draw"
