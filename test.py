9f7b40f0ddb5315f890150497df9beea45162ff2
kuwala <kuwala@gmail.com>
2016-01-05 23:34:18 -0800

    added keyboard interrupt

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from oscServ import oscServerGuy
 from models import synthState
 
 print("Pygame RpiSynth test started")
+
 savedOSC = synthState()
 oscGuy = oscServerGuy(savedOSC)
 
@@ -15,8 +16,14 @@ keyboardController = events.KeyboardController(evManager)
 cpuSpinner = events.CPUSpinner(evManager)
 
 view = PygameView(evManager)
+# run the visuals as fast as possible
+# but catch KeyboardInterrupt as to not
+# leave a thread hanging when force closed
 
-cpuSpinner.run()
+try :
+  cpuSpinner.run()
+except KeyboardInterrupt:
+  evManager.post(events.QuitEvent())
 
 oscGuy.close()
 print("The test is now over")
